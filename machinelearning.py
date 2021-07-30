6993b931b422ee0e7350923ddc8432269ba761d3
Mikail22 <73679712+Mikail22@users.noreply.github.com>
2021-07-30 18:51:08 +0800

    Update machinelearning.py

--- machinelearning.py
+++ machinelearning.py
@@ -60,3 +60,132 @@ def process_message(message, lower_case = True, stem = True, stop_words = True, 
         stemmer = PorterStemmer()
         words = [stemmer.stem(word) for word in words]   
     return words
+
+#Train the model
+class SpamClassifier(object):
+    def __init__(self, trainData, method = 'tf-idf'):
+        self.x, self.labels = trainData['url'], trainData['label']
+        self.method = method
+
+    def train(self):
+        self.calc_TF_and_IDF()
+        if self.method == 'tf-idf':
+            self.calc_TF_IDF()
+        else:
+            self.calc_prob()
+
+    def calc_prob(self):
+        self.prob_spam = dict()
+        self.prob_ham = dict()
+        for word in self.tf_spam:
+            self.prob_spam[word] = (self.tf_spam[word] + 1) / (self.spam_words + \
+                                                                len(list(self.tf_spam.keys())))
+        for word in self.tf_ham:
+            self.prob_ham[word] = (self.tf_ham[word] + 1) / (self.ham_words + \
+                                                                len(list(self.tf_ham.keys())))
+        self.prob_spam_mail, self.prob_ham_mail = self.spam_mails / self.total_mails, self.ham_mails / self.total_mails 
+
+
+    def calc_TF_and_IDF(self):
+        noOfMessages = self.x.shape[0]
+        self.spam_mails, self.ham_mails = self.labels.value_counts()[1], self.labels.value_counts()[0]
+        self.total_mails = self.spam_mails + self.ham_mails
+        self.spam_words = 0
+        self.ham_words = 0
+        self.tf_spam = dict()
+        self.tf_ham = dict()
+        self.idf_spam = dict()
+        self.idf_ham = dict()
+        for i in range(noOfMessages):
+            message_processed = process_message(self.x[i])
+            count = list() #To keep track of whether the word has ocured in the message or not.
+                           #For IDF
+            for word in message_processed:
+                if self.labels[i]:
+                    self.tf_spam[word] = self.tf_spam.get(word, 0) + 1
+                    self.spam_words += 1
+                else:
+                    self.tf_ham[word] = self.tf_ham.get(word, 0) + 1
+                    self.ham_words += 1
+                if word not in count:
+                    count += [word]
+            for word in count:
+                if self.labels[i]:
+                    self.idf_spam[word] = self.idf_spam.get(word, 0) + 1
+                else:
+                    self.idf_ham[word] = self.idf_ham.get(word, 0) + 1
+
+    def calc_TF_IDF(self):
+        self.prob_spam = dict()
+        self.prob_ham = dict()
+        self.sum_tf_idf_spam = 0
+        self.sum_tf_idf_ham = 0
+        for word in self.tf_spam:
+            self.prob_spam[word] = (self.tf_spam[word]) * log((self.spam_mails + self.ham_mails) \
+                                                          / (self.idf_spam[word] + self.idf_ham.get(word, 0)))
+            self.sum_tf_idf_spam += self.prob_spam[word]
+        for word in self.tf_spam:
+            self.prob_spam[word] = (self.prob_spam[word] + 1) / (self.sum_tf_idf_spam + len(list(self.prob_spam.keys())))
+            
+        for word in self.tf_ham:
+            self.prob_ham[word] = (self.tf_ham[word]) * log((self.spam_mails + self.ham_mails) \
+                                                          / (self.idf_spam.get(word, 0) + self.idf_ham[word]))
+            self.sum_tf_idf_ham += self.prob_ham[word]
+        for word in self.tf_ham:
+            self.prob_ham[word] = (self.prob_ham[word] + 1) / (self.sum_tf_idf_ham + len(list(self.prob_ham.keys())))
+            
+    
+        self.prob_spam_mail, self.prob_ham_mail = self.spam_mails / self.total_mails, self.ham_mails / self.total_mails 
+                    
+    def classify(self, processed_message):
+        pSpam, pHam = 0, 0
+        for word in processed_message:                
+            if word in self.prob_spam:
+                pSpam += log(self.prob_spam[word])
+            else:
+                if self.method == 'tf-idf':
+                    pSpam -= log(self.sum_tf_idf_spam + len(list(self.prob_spam.keys())))
+                else:
+                    pSpam -= log(self.spam_words + len(list(self.prob_spam.keys())))
+            if word in self.prob_ham:
+                pHam += log(self.prob_ham[word])
+            else:
+                if self.method == 'tf-idf':
+                    pHam -= log(self.sum_tf_idf_ham + len(list(self.prob_ham.keys()))) 
+                else:
+                    pHam -= log(self.ham_words + len(list(self.prob_ham.keys())))
+            pSpam += log(self.prob_spam_mail)
+            pHam += log(self.prob_ham_mail)
+        return pSpam >= pHam
+    
+    def predict(self, testData):
+        result = dict()
+        for (i, message) in enumerate(testData):
+            processed_message = process_message(message)
+            result[i] = int(self.classify(processed_message))
+        return result
+
+    def metrics(labels, predictions):
+    true_pos, true_neg, false_pos, false_neg = 0, 0, 0, 0
+    for i in range(len(labels)):
+        true_pos += int(labels[i] == 1 and predictions[i] == 1)
+        true_neg += int(labels[i] == 0 and predictions[i] == 0)
+        false_pos += int(labels[i] == 0 and predictions[i] == 1)
+        false_neg += int(labels[i] == 1 and predictions[i] == 0)
+    precision = true_pos / (true_pos + false_pos)
+    recall = true_pos / (true_pos + false_neg)
+    Fscore = 2 * precision * recall / (precision + recall)
+    accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
+
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F-score: ", Fscore)
+    print("Accuracy: ", accuracy)
+    
+#Final Results
+sc_tf_idf = SpamClassifier(trainData, 'tf-idf')
+sc_tf_idf.train()
+preds_tf_idf = sc_tf_idf.predict(testData['url'])
+metrics(testData['label'], preds_tf_idf)
+
+
